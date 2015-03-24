03d731b92445af4c3653e2ff6a781e1bdc1866af
Ranjit <ran89here@gmail.com>
2015-03-23 20:04:49 -0700

    test

---
+++ src/Nancy.Tests/Unit/Diagnostics/InteractiveDiagnosticsFixture.cs
@@ -0,0 +1,60 @@
+﻿using Nancy.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Nancy.Tests.Unit.Diagnostics
+{
+    class InteractiveDiagnosticsFixture
+    {
+        private class FakeParentDiagnosticsProvider : IDiagnosticsProvider
+        {
+            public string Name
+            {
+                get { return "Fake testing provider"; }
+            }
+
+            public string Description
+            {
+                get { return "Fake testing provider"; }
+            }
+
+            public object DiagnosticObject
+            {
+                get { return this; }
+            }
+
+            [Description("Parent public method.")]
+            [Template("<h1>{{model.Result}}</h1>")]
+            public string ParentPublicMethod()
+            {
+                return string.Concat("In parent public.");
+            }
+        }
+
+        private class FakeChildDiagnosticsProvider : FakeParentDiagnosticsProvider 
+        {
+            [Description("Child public method.")]
+            [Template("<h1>{{model.Result}}</h1>")]
+            public string ParentPublicMethod()
+            {
+                return string.Concat("In child public.");
+            }
+        }
+
+        [Fact]
+        public void Should_return_return_methods_from_entire_hierarchy() 
+        {
+            var child = new FakeChildDiagnosticsProvider();
+            var parent = new FakeParentDiagnosticsProvider();
+            IDiagnosticsProvider[] arr = new IDiagnosticsProvider[] {child, parent };
+            IEnumerable<IDiagnosticsProvider> ie = arr;
+            var id = new InteractiveDiagnostics(ie);
+            var ad = id.AvailableDiagnostics;
+            var a = 0;
+        }
+
+    }
+}
